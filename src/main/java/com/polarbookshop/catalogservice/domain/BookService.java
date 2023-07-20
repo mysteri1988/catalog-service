29edf916e6474094a799a28053768788fa0e3291
mysteri1988 <mysteri1988@gmail.com>
2023-07-20 19:23:18 +0200

    Fix bugs

--- src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
@@ -1,12 +1,7 @@
 package com.polarbookshop.catalogservice.domain;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Service
 public class BookService {
